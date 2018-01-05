9e0404c8b492089827d6d1abb5c4dd57a1d65f11
Ted Shin <tdyshin@gmail.com>
2018-01-04 23:44:25 -0500

    Set up MyBooks and AllBooks components

---
+++ client/src/components/AllBooks.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import axios from 'axios';
+import { Container } from 'semantic-ui-react';
+
+class AllBooks extends Component {
+
+  render() {
+    return (
+      <Container>
+        Allbooks
+      </Container>
+    );
+  }
+}
+
+// Create a new component that is "connected" to the router.
+export default (AllBooks)
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 // import Home from './Home';
-// import AllBooks from './AllBooks';
-// import MyBooks from './MyBooks';
+import AllBooks from './AllBooks';
+import MyBooks from './MyBooks';
 import Login from './Login';
 import Signup from './Signup';
 
@@ -12,12 +12,18 @@ export default class Main extends Component {
     return (
       <main>
         <Switch>
+          <Route exact path='/allbooks' render={(props) => {
+            return <AllBooks {...props} />
+          }} />
+          <Route exact path='/mybooks' render={(props) => {
+            return <MyBooks {...props} />
+          }} />
           <Route exact path='/login' render={(props) => {
             return <Login {...props} />
           }} />
-            <Route exact path='/signup' render={(props) => {
-              return <Signup {...props} />
-            }} />
+          <Route exact path='/signup' render={(props) => {
+            return <Signup {...props} />
+          }} />
         </Switch>
       </main>
     );
